d14988218c57ef593d83910ef3b52bc0ba6de58b
jpalafox12 <53842278+jpalafox12@users.noreply.github.com>
2023-06-10 13:06:44 -0600

    prueba15

--- config.php
+++ config.php
@@ -7,18 +7,12 @@ $DB_PORT = $_ENV["DB_PORT"];
 
 $mysqli  = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
-//$mysqli = new mysqli("localhost", "root", "", "prueba");
-
 if ($mysqli->connect_errno) {
   echo json_encode(1);
-  die("Error al conectar a la base de datos: " );
-}else{
-  //echo json_encode(2);
-  /*
-$mysqli = new mysqli("http://localhost:3306", "id20717297_wilson", "Wilson2017!", "id20717297_tt");
-//$mysqli-> set_charset("utf8");
-
-if ($mysqli->connect_errno) {
-  die("Error al conectar a la base de datos: " );
-}else{}*/
+  die("Error al conectar a la base de datos: " . $mysqli->connect_error);
+} else {
+  // La conexión a la base de datos se ha establecido correctamente
+  echo json_encode(2);
 }
+?>
+
